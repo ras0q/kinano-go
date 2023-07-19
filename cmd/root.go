85b9ad773db59a25bb9af9b3b4b671fde4361e38
ras0q <ras0q@users.noreply.github.com>
2023-07-19 15:56:05 +0900

    :sparkles: impl rootcmd.Run

--- cmd/root.go
+++ cmd/root.go
@@ -35,16 +35,10 @@ var ErrCommandNotFound = fmt.Errorf("command not found")
 func rootCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "kinano-go",
-		Short: "A brief description of your application",
-		Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-		// Uncomment the following line if your bare application
-		// has an action associated with it:
-		// Run: func(cmd *cobra.Command, args []string) { },
+		Short: "I am kinano v2",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Println("oisu-")
+		},
 	}
 }
 
